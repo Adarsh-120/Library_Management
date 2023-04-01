f6641f89c40e06a562945ce1d6015c5e36eb9b99
Adarsh-1207 <Adarsh20134@gmail.com>
2023-04-01 15:20:02 +0530

    express

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+
+const app = express();
+
+const PORT = 8081;
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.status(202).json({
+    message: "Server is up and running",
+  });
+});
+app.listen(PORT, () => {
+  console.log(`Server is running at port: ${PORT}`);
+});
