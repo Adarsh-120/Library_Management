bf46585a5f09c3b296db5c53ecbb4aca670f3c45
Adarsh Kumar Choudhary <128061818+Adarsh-120@users.noreply.github.com>
2023-04-02 06:17:46 +0000

    SetupDone

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const { users } = require("./data/users.json");
 
 const app = express();
 
@@ -7,7 +8,30 @@ app.use(express.json());
 
 app.get("/", (req, res) => {
   res.status(202).json({
+    //In place of json we can use send but json help to update more than one
+    // time data's or message .It can automatically update the new added lines.
     message: "Server is up and running",
+  });
+});
+
+/**
+ * Route: /users
+ * Method: GET
+ * Description: Get al users
+ * Access: Public
+ * Parameters: None
+ */
+
+app.get("/users", (req, res) => {
+  res.status(200).json({
+    success: true,
+    data: users,
+  });
+});
+
+app.get("*", (req, res) => {
+  res.status(404).json({
+    message: "This route does not exist",
   });
 });
 app.listen(PORT, () => {
